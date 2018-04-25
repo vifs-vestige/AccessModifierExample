6c486d2001e31f6bf0054a687b11201ab98ae57f
ShawnSwinsick <vifs888@gmail.com>
2018-04-25 17:20:00 -0600

    Added private

---
+++ AccessModifierExample/PrivateModifier.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifierExample
+{
+    class PrivateModifier
+    {
+
+        private static void PrivateMethod()
+        {
+            Console.WriteLine("Called a private method");
+        }
+    }
+
+    class TestPrivateSubClass : PrivateModifier
+    {
+        public TestPrivateSubClass()
+        {
+            PrivateModifier.PrivateMethod();
+        }
+    }
+
+    class TestPrivate
+    {
+        public TestPrivate()
+        {
+            PrivateModifier.PrivateMethod();
+        }
+    }
+}
