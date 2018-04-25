ca115a88ba7d70e7d6a578e653399d84871a9c9e
ShawnSwinsick <vifs888@gmail.com>
2018-04-25 17:05:01 -0600

    First check in, public stuff added

---
+++ AccessModifierExample/PublicModifier.cs
@@ -0,0 +1,40 @@
+﻿using AccessModifierExample;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifierExample
+{
+    public class PublicModifier
+    {
+
+        public static void PublicMethod()
+        {
+            Console.WriteLine("Called a public method");
+        }
+    }
+
+    public class TestPublicSubClass : PublicModifier
+    {
+        PublicModifier.PublicMethod();
+    }
+
+    public class TestPublicInsideNamespace
+    {
+        public TestPublicInsideNamespace()
+        {
+            PublicModifier.PublicMethod();
+        }
+    }
+}
+
+public class TestPublicOutsideNamespace
+{
+    public TestPublicOutsideNamespace()
+    {
+        PublicModifier.PublicMethod();
+    }
+}
+
